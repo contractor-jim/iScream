a22d8d44274b2c0e6d9ba2d5295a1182b7f309bc
James Woodbridge <JWOODBRI@partner.jaguarlandrover.com>
2025-08-26 09:59:51 +0100

    Added initial xCode project

---
+++ iScream/iScream/iScreamApp.swift
@@ -0,0 +1,32 @@
+//
+//  iScreamApp.swift
+//  iScream
+//
+//  Created by james Woodbridge on 26/08/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct iScreamApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
