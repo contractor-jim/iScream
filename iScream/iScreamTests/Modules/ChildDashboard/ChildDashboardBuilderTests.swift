e2a7885bdc0f898bbe73c8b200a679dc2c9d7e2f
James Woodbridge <JWOODBRI@partner.jaguarlandrover.com>
2025-09-01 10:44:30 +0100

    Added child dashboard builder tests

---
+++ iScream/iScreamTests/Modules/ChildDashboard/ChildDashboardBuilderTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChildDashboardBuilderTests.swift
+//  iScream
+//
+//  Created by James Woodbridge on 01/09/2025.
+//
+
+import Testing
+@testable import iScream
+
+struct ChildDashboardDefaultBuilderTests {
+
+    var mockUserService: MockUserService
+    let router: ChildDashboardRouter
+    let interactor: ChildDashboardInteractor
+    let presenter: ChildDashboardPresenterImp
+
+    init() throws {
+        mockUserService = MockUserService()
+        router = ChildDashboardRouterImp()
+        interactor = ChildDashboardInteractorImp(entity: ChildDashboardEntityImp(), userService: mockUserService)
+        presenter = ChildDashboardPresenterImp(interactor: interactor, router: router)
+    }
+
+    @Test("POSITIVE - Can create a child dashboard module") func testFetch() throws {
+        let builder = ChildDashboardDefaultBuilder()
+        let _ = builder.buildChildDashboardView()
+        try #require(builder.container.resolve(ChildDashboardEntity.self) != nil)
+        try #require(builder.container.resolve(ChildDashboardInteractor.self) != nil)
+        try #require(builder.container.resolve(ChildDashboardRouter.self) != nil)
+        try #require(builder.container.resolve(ChildDashboardPresenter.self) != nil)
+        try #require(builder.container.resolve(ChildDashboardView.self) != nil)
+        // TODO: When the opaque / boxing of the views look at this
+//        try #require(testView as? RootContainerView != nil)
+    }
+}
