d4dd1651712b485c81191738f215c904ea1d58ad
James Woodbridge <JWOODBRI@partner.jaguarlandrover.com>
2025-09-01 10:30:18 +0100

    Added tests for parent child list interactor

--- iScream/iScream/Modules/ParentListChildren/Interactor/ParentListChildrenInteractor.swift
+++ iScream/iScream/Modules/ParentListChildren/Interactor/ParentListChildrenInteractor.swift
@@ -9,24 +9,24 @@ import SwiftUI
 
 protocol ParentListChildrenInteractor {
     init(entity: ParentListChildrenEntity,
-         userService: UserService)
+         userService: any UserService)
 
     var entity: ParentListChildrenEntity! { get }
     var userService: UserService! { get set }
-    
+
     func fetchMyUser() async -> User
 }
 
 class ParentListChildrenInteractorImp: ParentListChildrenInteractor {
     let entity: ParentListChildrenEntity!
     var userService: UserService!
-    // TODO: Test this
+
     required init(entity: any ParentListChildrenEntity,
-                  userService: UserService) {
+                  userService: any UserService) {
         self.entity = entity
         self.userService = userService
     }
-    // TODO: Test this
+
     func fetchMyUser() async -> User {
         return await userService.getUser()
     }
