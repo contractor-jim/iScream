a605c30146c62d27d408bc79eae674b4abfa1743
James Woodbridge <JWOODBRI@partner.jaguarlandrover.com>
2025-09-05 10:13:03 +0100

    Removed apply prov prof

--- iScream/Untitled.swift
+++ iScream/Untitled.swift
@@ -4,9 +4,9 @@
 //
 //  Created by james Woodbridge on 05/09/2025.
 //
-
-run: |
-
-- name: Build and Test
-run: |
-  xcodebuild clean test -project iScream.xcodeproj -scheme "iScream" -configuration Staging -destination 'platform=iOS Simulator,name=iPhone 16'
+//
+//run: |
+//
+//- name: Build and Test
+//run: |
+//  xcodebuild clean test -project iScream.xcodeproj -scheme "iScream" -configuration Staging -destination 'platform=iOS Simulator,name=iPhone 16'
