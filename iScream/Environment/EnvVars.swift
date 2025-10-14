a2bf81108af637805cbe24d3bef6504af0bcd761
James Woodbridge <james.woodbrdge@gmail.com>
2025-10-14 09:46:28 +0100

    Enforced removal of vars

--- iScream/Environment/EnvVars.swift
+++
@@ -1,9 +0,0 @@
-// Generated, do not create manually
-
-import Foundation
-
-struct EnvVars {
-	static let envSupabaseUrl = "ENV_SUPABASE_URL"
-	static let envSupabaseAuthKey = "ENV_SUPABASE_AUTH_KEY"
-
-}
