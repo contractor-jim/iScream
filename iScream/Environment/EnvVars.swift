a00746bb898043e191c3e9bc9a3c83217b77f9cf
James Woodbridge <james.woodbrdge@gmail.com>
2025-10-14 09:43:57 +0100

    Added tests to gen default env vars

---
+++ iScream/Environment/EnvVars.swift
@@ -0,0 +1,9 @@
+// Generated, do not create manually
+
+import Foundation
+
+struct EnvVars {
+	static let envSupabaseUrl = "ENV_SUPABASE_URL"
+	static let envSupabaseAuthKey = "ENV_SUPABASE_AUTH_KEY"
+
+}
