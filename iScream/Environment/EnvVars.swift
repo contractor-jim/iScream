05194a6d239b387973dfd8d9b6367db859dd5bbd
contractor-jim <47974377+contractor-jim@users.noreply.github.com>
2025-10-14 10:46:58 +0100

    FIX: Remove env vars file (#57)
    * Removed vars
    
    * Removed redundant config folder
    
    * Added tests to gen default env vars
    
    * Enforced removal of vars
    
    * Removed env vars
    
    * Removed the built script
    
    * Adde dempty var gen to the CI script to break it out
    
    * Added script to make default env vars
    
    ---------
    
    Co-authored-by: James Woodbridge <james.woodbrdge@gmail.com>

--- iScream/Environment/EnvVars.swift
+++
@@ -1,9 +0,0 @@
-// Generated, do not create manually
-
-import Foundation
-
-struct EnvVars {
-	static let envSupabaseAuthKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cXRlanJreXF5enF3em5ram56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNzUyMTgsImV4cCI6MjA3Mzg1MTIxOH0.SKutFFeqA_XU-qf36Cqs0EwO0v1KfzUHxSL06tkPlNg"
-	static let envSupabaseUrl = "https://dwqtejrkyqyzqwznkjnz.supabase.co"
-
-}
